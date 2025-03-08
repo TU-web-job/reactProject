f57d62647b70ccac5ed802a9572c90dbc6e67a35
{tu-web-job} <{t.u.9293991@gmail.com}>
2025-03-08 16:26:53 +0900

    initToPage

--- src/app/init/page.tsx
+++ src/app/init/page.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { HeaderWrapper } from "myapp/component/Header";
 import { FooterComponent } from "myapp/component/Footer";
-import { Main } from "myapp/component/Main"
-export default function Home() {
+import { Main } from "next/document";
+
+export function Home() {
   return (
-    <>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <HeaderWrapper HeadText="Page!!" />
+      <HeaderWrapper HeadText="Init Page!"/>
       <Main />
       <FooterComponent />
     </div>
-    </>
   );
 }
